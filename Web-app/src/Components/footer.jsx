d7c65500916e996b32da1901eb983ef4b448189a
AyoOlu1 <oluwayemivictor15@gmail.com>
2023-10-13 20:42:35 +0100

    edit header and footer

--- Web-app/src/Components/footer.jsx
+++ Web-app/src/Components/footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <>
-      <footer></footer>
+      <footer className=" bg-[#2A2954]"></footer>
     </>
   );
 }
--- Web-app/src/Components/header.jsx
+++ Web-app/src/Components/header.jsx
@@ -1,28 +1,39 @@
 import { Link } from "react-router-dom";
 import logo from "/assets/logo.png";
+import menu from "/assets/menu.png";
 
 function Header() {
   return (
     <>
-      <header className="flex-between mx-auto py-6 max-w-[1140px] ">
-        <div>
-          <img src={logo} alt="" />
-        </div>
-        {/* desktop nav */}
-        <nav className=" ">
+      <header className="bg-[#EFECF5] px-8">
+        <div className=" flex-between mx-auto py-6 max-w-[1140px] ">
           <div>
-            <div>
-              <a href="#">Features</a>
-              <a href="#">How it Works</a>
-              <a href="#">Pricing</a>
-              <a href="#">Suport</a>
-            </div>
+            <img src={logo} alt="" />
           </div>
-        </nav>
+          {/* desktop nav */}
+          <nav className="hidden lg:flex ">
+            <div>
+              <div>
+                <a href="#">Features</a>
+                <a href="#">How it Works</a>
+                <a href="#">Pricing</a>
+                <a href="#">Suport</a>
+              </div>
+            </div>
+          </nav>
 
-        <Link to="/login" className="">
-          Login
-        </Link>
+          <Link
+            to="/login"
+            className="hidden lg:block rounded-lg border-[#656565] border-[1px] py-[18px] px-14"
+          >
+            Login
+          </Link>
+
+          {/* harmburger menu */}
+          <div className=" lg:hidden ">
+            <img src={menu} alt="" />
+          </div>
+        </div>
       </header>
     </>
   );
