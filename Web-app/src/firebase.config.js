a21f28bad5d8bac9a9e8406a6b0ce28341edd4ea
AyoOlu1 <oluwayemivictor15@gmail.com>
2023-10-16 09:12:14 +0100

    add firebase config

---
+++ Web-app/src/firebase.config.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: import.meta.env.REACT_APP_FIREBASE_KEY,
+    authDomain: import.meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: import.meta.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  };
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+const goggleAuthProvider = new GoogleAuthProvider();
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(firebaseApp);
+
+export { auth, goggleAuthProvider, db };
