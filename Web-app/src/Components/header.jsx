05ecb5b2a904979b85f6001357b19f7bf0b39f9f
AyoOlu1 <oluwayemivictor15@gmail.com>
2023-10-16 09:13:43 +0100

    add mobile nav

--- Web-app/src/Components/header.jsx
+++ Web-app/src/Components/header.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import logo from "/assets/logo.png";
 import menu from "/assets/menu.png";
+import { useState } from "react";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <header className="bg-[#EFECF5] px-8">
@@ -31,7 +33,29 @@ function Header() {
 
           {/* harmburger menu */}
           <div className=" lg:hidden ">
-            <img src={menu} alt="" />
+            <img src={menu} onClick={() => setIsOpen(!isOpen)} alt="" />
+          </div>
+
+          {/* mobile nav */}
+          <div
+            className={`${
+              !isOpen ? "hidden" : "block w-[80vw] md:w-[50vw] transition-all"
+            } 
+              lg:hidden drop-shadow-mobile-nav
+           border-[#E1E6EF] border-y-[0.298611px]  
+           border-solid pb-10 pt-12 fixed bottom-0 overflow-auto top-[83px] right-0 bg-white z-10`}
+          >
+            <div className="px-4 pt-2 pb-3">
+              <h4>
+                <Link className="text-pry-blue-600 block mb-8 " to="/">
+                  Home
+                </Link>
+              </h4>
+
+              <h6 className="mt-[50px] mb-8 w-[217px]">
+                Navigating immigration? We have visa options tailored for you
+              </h6>
+            </div>
           </div>
         </div>
       </header>
