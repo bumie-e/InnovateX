f53d62de4e082cf3cf078beec90c0ccdccb37772
AyoOlu1 <oluwayemivictor15@gmail.com>
2023-10-16 02:57:32 +0100

    add favicon

--- Web-app/tailwind.config.js
+++ Web-app/tailwind.config.js
@@ -8,7 +8,11 @@ export default {
     extend: {
       colors: {
         "pry-col": "#5250CD",
-        "feature-bg": "linear-gradient(180deg, #FFF 0%, #FBF8F6 53.65%, #F4F1F7 100%)"
+        "feature-bg": "linear-gradient(180deg, #FFF 0%, #FBF8F6 53.65%, #F4F1F7 100%)",
+
+      },
+      boxShadow: {
+        "chat-onboarding": "2px 6px 36px 0px rgba(206, 202, 216, 0.25);"
       }
     },
   },
