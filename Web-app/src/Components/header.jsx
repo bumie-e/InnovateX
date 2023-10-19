7eca580cbba254c65b8ebde72c63e89df6dda735
AyoOlu1 <oluwayemivictor15@gmail.com>
2023-10-19 17:27:39 +0100

    edit header

--- Web-app/src/Components/header.jsx
+++ Web-app/src/Components/header.jsx
@@ -33,12 +33,21 @@ function Header() {
 
           {/* harmburger menu */}
           <div className=" lg:hidden ">
-            <img
-              src={menu}
-              width={32}
-              onClick={() => setIsOpen(!isOpen)}
-              alt=""
-            />
+            {!isOpen ? (
+              <img
+                src={menu}
+                width={32}
+                onClick={() => setIsOpen(!isOpen)}
+                alt=""
+              />
+            ) : (
+              <div
+                className="text-5xl text-pry-col"
+                onClick={() => setIsOpen(!isOpen)}
+              >
+                x
+              </div>
+            )}
           </div>
 
           {/* mobile nav */}
@@ -48,18 +57,26 @@ function Header() {
             } 
               lg:hidden drop-shadow-mobile-nav
            border-[#E1E6EF] border-y-[0.298611px]  
-           border-solid pb-10 pt-12 fixed bottom-0 overflow-auto top-[83px] right-0 bg-white z-10`}
+           border-solid pb-10 pt-12 fixed bottom-0 overflow-auto 
+           top-[70px] right-0 bg-white z-10`}
           >
             <div className="px-4 pt-2 pb-3">
-              <h4>
-                <Link className="text-pry-blue-600 block mb-8 " to="/">
+              <div className="flex flex-col items-start gap-8 ">
+                <Link className="text-pry-blue-600   " to="/">
                   Home
                 </Link>
-              </h4>
-
-              <h6 className="mt-[50px] mb-8 w-[217px]">
-                Navigating immigration? We have visa options tailored for you
-              </h6>
+                <Link to="#">Features</Link>
+                <Link to="#">How it Works</Link>
+                <Link to="#">Pricing</Link>
+                <Link to="#">Suport</Link>
+                <Link
+                  to="/login"
+                  className=" rounded-lg border-[#656565] 
+                  border-[1px] py-4 px-8 inline"
+                >
+                  Login
+                </Link>
+              </div>
             </div>
           </div>
         </div>
