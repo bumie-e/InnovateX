b78021044c5c8ce7de8c9ec697685976658b62c6
AyoOlu1 <oluwayemivictor15@gmail.com>
2023-10-30 15:18:46 +0100

    add quiz from the backend

--- Web-app/src/Components/quiz/mainQuiz.jsx
+++ Web-app/src/Components/quiz/mainQuiz.jsx
@@ -12,59 +12,101 @@ function MainQuiz() {
     explanation_type: "In-depth explorations",
     interaction_needed: "Yes",
   });
-  const handleStartQuiz = () => {
-    setShowQuiz(true);
-  };
+
+  const [quizData, setQuizData] = useState({});
   const [showQuiz, setShowQuiz] = useState(false);
-
-  const quizData = {
-    questions: {
-      "1. What is the time allowed for the exam?": {
-        options: ["a)1 hour", "b) 2 hours", "c) 3 hours", "d) 4 hours"],
-        answer: "a)1 hour",
-      },
-      "2. How many questions are required to be answered in total?": {
-        options: ["a) 2", "b) 3", "c) 4", "d) 5"],
-        answer: "c) 4",
-      },
-      "3. Which department offers the CPE401 course?": {
-        options: [
-          "a) Department of Computer Science",
-          "b) Department of Engineering",
-          "c) Department of Mathematics",
-          "d) Department of Business",
-        ],
-        answer: "a) Department of Computer Science",
-      },
-      "4. What is the purpose of using a different booklet for each section?": {
-        options: [
-          "a) To save paper",
-          "b) To make the exam more organized",
-          "c) To prevent cheating",
-          "d) To make it easier for the students",
-        ],
-        answer: "b) To make the exam more organized",
-      },
-      "5. What is the relationship between MIPS and CPI?": {
-        options: [
-          "a) Directly proportional",
-          "b) Inversely proportional",
-          "c) No relationship",
-          "d) Cannot be determined from the given information",
-        ],
-        answer: "d) Cannot be determined from the given information",
-      },
-    },
-  };
-
   const [correctAnswers, setCorrectAnswers] = useState(0);
   const [currentQuestion, setCurrentQuestion] = useState(1);
   const [selectedOptions, setSelectedOptions] = useState({});
   const [showModal, setShowModal] = useState(false);
+  useEffect(() => {
+    getQuiz();
+  }, []);
 
+  // const quizData = {
+  //   questions: {
+  //     "1. What is the time allowed for the exam?": {
+  //       options: ["a)1 hour", "b) 2 hours", "c) 3 hours", "d) 4 hours"],
+  //       answer: "a)1 hour",
+  //     },
+  //     "2. How many questions are required to be answered in total?": {
+  //       options: ["a) 2", "b) 3", "c) 4", "d) 5"],
+  //       answer: "c) 4",
+  //     },
+  //     "3. Which department offers the CPE401 course?": {
+  //       options: [
+  //         "a) Department of Computer Science",
+  //         "b) Department of Engineering",
+  //         "c) Department of Mathematics",
+  //         "d) Department of Business",
+  //       ],
+  //       answer: "a) Department of Computer Science",
+  //     },
+  //     "4. What is the purpose of using a different booklet for each section?": {
+  //       options: [
+  //         "a) To save paper",
+  //         "b) To make the exam more organized",
+  //         "c) To prevent cheating",
+  //         "d) To make it easier for the students",
+  //       ],
+  //       answer: "b) To make the exam more organized",
+  //     },
+  //     "5. What is the relationship between MIPS and CPI?": {
+  //       options: [
+  //         "a) Directly proportional",
+  //         "b) Inversely proportional",
+  //         "c) No relationship",
+  //         "d) Cannot be determined from the given information",
+  //       ],
+  //       answer: "d) Cannot be determined from the given information",
+  //     },
+  //   },
+  // };
+
+  const getQuiz = () => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append(
+      "Cookie",
+      "ARRAffinity=d3ae43c979c2cf911cdf1a2b87ef67e3ff3e7580ec1c1e247979dcdaa1f864d9; ARRAffinitySameSite=d3ae43c979c2cf911cdf1a2b87ef67e3ff3e7580ec1c1e247979dcdaa1f864d9"
+    );
+
+    var raw = JSON.stringify({
+      language: "English",
+      course_code: "CSC401",
+      page_number: 0,
+      topic: "jet engines",
+      explanation_level: "Advanced insights",
+      prior_knowledge: "Little",
+      explanation_type: "In-depth explorations",
+      interaction_needed: "Yes",
+    });
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch("https://innovatex.azurewebsites.net/quiz", requestOptions)
+      .then((response) => response.text())
+      .then((result) => {
+        if (result) {
+          // console.log(result);
+          const responseObject = JSON.parse(result);
+          // console.log(responseObject?.message);
+          setQuizData(responseObject?.message);
+        }
+      })
+      .catch((error) => console.log("error", error));
+  };
+  const handleStartQuiz = () => {
+    setShowQuiz(true);
+  };
   // console.log(quizData);
-  const questions = Object.keys(quizData.questions);
-  console.log(questions);
+  const questions = Object.keys(quizData?.questions || {});
+  // console.log(questions);
 
   const handleOptionSelect = (question, option) => {
     setSelectedOptions({ ...selectedOptions, [question]: option.trim() });
@@ -81,9 +123,9 @@ function MainQuiz() {
   const handleQuizSubmit = () => {
     let correctCount = 0; // Initialize a variable to count correct answers
     for (const question of questions) {
-      console.log(
-        selectedOptions[question] === quizData.questions[question].answer
-      );
+      // console.log(
+      //   selectedOptions[question] === quizData.questions[question].answer
+      // );
 
       if (selectedOptions[question] === quizData.questions[question].answer) {
         correctCount++; // Increment the count for each correct answer
@@ -92,7 +134,7 @@ function MainQuiz() {
 
     setCorrectAnswers(correctCount); // Update the correctAnswers state
 
-    console.log(correctCount);
+    // console.log(correctCount);
     setShowModal(true);
     return correctCount; // You can return the count if needed
   };
@@ -163,7 +205,7 @@ function MainQuiz() {
                 </div>
               </div>
             ) : (
-              ""
+              <h2>Loading...</h2>
             )
           ) : (
             "Loading"
