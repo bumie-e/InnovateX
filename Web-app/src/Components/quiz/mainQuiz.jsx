51c044d24ce18959dad4cc2528798039cd73cdf0
AyoOlu1 <oluwayemivictor15@gmail.com>
2023-10-30 12:08:40 +0100

    edit chat page and fix quiz page

--- Web-app/src/Components/quiz/mainQuiz.jsx
+++ Web-app/src/Components/quiz/mainQuiz.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import QuizForm from "./quizForm";
 
@@ -21,7 +21,7 @@ function MainQuiz() {
     questions: {
       "1. What is the time allowed for the exam?": {
         options: ["a)1 hour", "b) 2 hours", "c) 3 hours", "d) 4 hours"],
-        answer: "a) 1 hour",
+        answer: "a)1 hour",
       },
       "2. How many questions are required to be answered in total?": {
         options: ["a) 2", "b) 3", "c) 4", "d) 5"],
@@ -56,12 +56,15 @@ function MainQuiz() {
       },
     },
   };
+
   const [correctAnswers, setCorrectAnswers] = useState(0);
   const [currentQuestion, setCurrentQuestion] = useState(1);
   const [selectedOptions, setSelectedOptions] = useState({});
   const [showModal, setShowModal] = useState(false);
 
+  // console.log(quizData);
   const questions = Object.keys(quizData.questions);
+  console.log(questions);
 
   const handleOptionSelect = (question, option) => {
     setSelectedOptions({ ...selectedOptions, [question]: option.trim() });
@@ -102,65 +105,70 @@ function MainQuiz() {
             <h3>Differential Calculus Quiz</h3>
             <span>5 questions</span>
           </div>
-          {currentQuestion <= questions.length ? (
-            <div>
-              <h3 className="font-bold text-lg mb-4">
-                {questions[currentQuestion - 1]}
-              </h3>
-              {quizData.questions[questions[currentQuestion - 1]].options.map(
-                (option, index) => (
-                  <div key={index}>
-                    <input
-                      type="radio"
-                      name={`q${currentQuestion}`}
-                      value={option}
-                      checked={
-                        selectedOptions[questions[currentQuestion - 1]] ===
-                        option.trim()
-                      }
-                      onChange={() =>
-                        handleOptionSelect(
-                          questions[currentQuestion - 1],
-                          option
-                        )
-                      }
-                    />
-                    {option}
-                  </div>
-                )
-              )}
-              <div className="flex-between mt-4">
-                <button
-                  className="border-[1px] border-pry-col rounded-lg px-9 py-[19px]"
-                  onClick={handlePrevious}
-                  disabled={currentQuestion === 1}
-                >
-                  Previous
-                </button>
-                {currentQuestion === questions.length ? (
-                  <div>
-                    <button
-                      className="bg-pry-col py-2 px-8 lg:py-[18px] lg:px-20 
-                  rounded-lg text-white "
-                      onClick={handleQuizSubmit}
-                    >
-                      Submit
-                    </button>
-                  </div>
-                ) : (
+          {quizData ? (
+            currentQuestion <= questions.length ? (
+              <div>
+                <h3 className="font-bold text-lg mb-4">
+                  {questions[currentQuestion - 1]}
+                </h3>
+                {quizData.questions[questions[currentQuestion - 1]].options.map(
+                  (option, index) => (
+                    <div key={index}>
+                      <input
+                        type="radio"
+                        name={`q${currentQuestion}`}
+                        value={option}
+                        checked={
+                          selectedOptions[questions[currentQuestion - 1]] ===
+                          option.trim()
+                        }
+                        onChange={() =>
+                          handleOptionSelect(
+                            questions[currentQuestion - 1],
+                            option
+                          )
+                        }
+                      />
+                      {option}
+                    </div>
+                  )
+                )}
+                <div className="flex-between mt-4">
                   <button
                     className="border-[1px] border-pry-col rounded-lg px-9 py-[19px]"
-                    onClick={handleNext}
-                    disabled={currentQuestion === questions.length}
+                    onClick={handlePrevious}
+                    disabled={currentQuestion === 1}
                   >
-                    Next
+                    Previous
                   </button>
-                )}
+                  {currentQuestion === questions.length ? (
+                    <div>
+                      <button
+                        className="bg-pry-col py-2 px-8 lg:py-[18px] lg:px-20 
+                  rounded-lg text-white "
+                        onClick={handleQuizSubmit}
+                      >
+                        Submit
+                      </button>
+                    </div>
+                  ) : (
+                    <button
+                      className="border-[1px] border-pry-col rounded-lg px-9 py-[19px]"
+                      onClick={handleNext}
+                      disabled={currentQuestion === questions.length}
+                    >
+                      Next
+                    </button>
+                  )}
+                </div>
               </div>
-            </div>
+            ) : (
+              ""
+            )
           ) : (
-            ""
+            "Loading"
           )}
+
           {showModal && (
             // <div className="modal">
             //   <div className="modal-content">
